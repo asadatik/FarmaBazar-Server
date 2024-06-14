d15b3934db783764bcc3f3568ac5cbd4f6c776ae
Asad Atik <asadatik1995@gmail.com>
2024-06-15 00:32:16 +0600

    Category Post Method Added

--- index.js
+++ index.js
@@ -27,63 +27,63 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-              // collections
-    const CategoryCardCollection  = client.db('FarmaBazar').collection('category')
-    const MedicineCollection  = client.db('FarmaBazar').collection('allMedicine')
-    const SelectedMedicine  = client.db('FarmaBazar').collection('ordered')
-    const userCollection  = client.db('FarmaBazar').collection('users')
+    // collections
+    const CategoryCardCollection = client.db('FarmaBazar').collection('category')
+    const MedicineCollection = client.db('FarmaBazar').collection('allMedicine')
+    const SelectedMedicine = client.db('FarmaBazar').collection('ordered')
+    const userCollection = client.db('FarmaBazar').collection('users')
 
- /////////USER RELETED API
+    /////////USER RELETED API
 
-//updated user role 
- app.patch('/users/user/:id',  async (req, res) => {
-  const id = req.params.id;
-  const filter = { _id: new ObjectId(id) };
-  const updatedDoc = {
-    $set: {
-      role: 'user'
-    }
-  }
-  const result = await userCollection.updateOne(filter, updatedDoc);
-  res.send(result);
-})
+    //updated user role 
+    app.patch('/users/user/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'user'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
 
-//updated seller role 
-app.patch('/users/seller/:id',  async (req, res) => {
-  const id = req.params.id;
-  const filter = { _id: new ObjectId(id) };
-  const updatedDoc = {
-    $set: {
-      role: 'seller'
-    }
-  }
-  const result = await userCollection.updateOne(filter, updatedDoc);
-  res.send(result);
-})
+    //updated seller role 
+    app.patch('/users/seller/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'seller'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
 
-//updated Admin role
-app.patch('/users/admin/:id',  async (req, res) => {
-  const id = req.params.id;
-  const filter = { _id: new ObjectId(id) };
-  const updatedDoc = {
-    $set: {
-      role: 'admin'
-    }
-  }
-  const result = await userCollection.updateOne(filter, updatedDoc);
-  res.send(result);
-})
-
-
+    //updated Admin role
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
 
 
 
-// post user info
-  
+
+
+ // post user info
+
     app.post('/users', async (req, res) => {
       const user = req.body;
       console.log(user)
-      const query = { email : user.email }
+      const query = { email: user.email }
       const existingUser = await userCollection.findOne(query);
       if (existingUser) {
         return res.send({ message: 'user already exists', insertedId: null })
@@ -91,89 +91,96 @@ app.patch('/users/admin/:id',  async (req, res) => {
 
       const result = await userCollection.insertOne(user);
       res.send(result);
-    });  
-   //get 
-   app.get('/users', async (req, res) => {
-    const result = await userCollection.find().toArray()
+    });
+    //get 
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray()
 
-    res.send(result)
-   })  
-    
-      
+      res.send(result)
+    })
+
+
  ////CATEGORY ////  
 
-//get 
+    // add
+    app.post('/category', async (req, res) => {
+      const cartItem = req.body;
+      const result = await CategoryCardCollection.insertOne(cartItem);
+      res.send(result);
+    });
+
+    //get 
     app.get('/category', async (req, res) => {
-     const result = await CategoryCardCollection.find().toArray()
+      const result = await CategoryCardCollection.find().toArray()
 
-     res.send(result)
-    }) 
+      res.send(result)
+    })
 
-// Delete
+    // Delete
     app.delete('/category/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await CategoryCardCollection.deleteOne(query);
       res.send(result);
     })
-   // Updated Oparetion
+    // Updated Oparetion
 
-   app.put("/category/:id", async (req, res) => {
-    const id = req.params.id;
-    const query = { _id: new ObjectId(id) };
-    const options = { upsert: true };
-    const updateSpot = {
-      $set: {
-        name: req.body.name,
-        image:req.body.image,
-        itemCount:req.body.itemCount
-      },
-    };
-    const result = await CategoryCardCollection.updateOne(
-      query,
-      updateSpot,
-      options
-    );
-    res.send(result);
-    console.log(id, query, result);
-  });
-
-
+    app.put("/category/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateSpot = {
+        $set: {
+          name: req.body.name,
+          image: req.body.image,
+          itemCount: req.body.itemCount
+        },
+      };
+      const result = await CategoryCardCollection.updateOne(
+        query,
+        updateSpot,
+        options
+      );
+      res.send(result);
+      console.log(id, query, result);
+    });
 
 
- ////   ALL MEDICINE     ////  
 
-//get   
 
- app.get('/allmedi', async (req, res) => {
-  const result = await MedicineCollection.find().toArray()
-  res.send(result)
- }) 
+    ////   ALL MEDICINE     ////  
 
- //// Added selected data in dataBASE
- app.post('/carts', async (req, res) => {
-  const cartItem = req.body;
-  const result = await SelectedMedicine.insertOne(cartItem);
-  res.send(result);
-});
+    //get   
 
-  // Get all Selected MEDICINE data from db
-  app.get('/carts', async (req, res) => {
-    const email = req.query.email; 
-    
-        console.log(email, "vejalllllllllllllllllllllllllllllllllllllllllllllllllllll" )
-       const query = { email : email };
-        const result = await SelectedMedicine.find(query).toArray();
-        res.send(result);
-      });
-  // delete SELECTED dataaaaaa
-      app.delete('/carts/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) }
-        const result = await SelectedMedicine.deleteOne(query);
-        res.send(result);
-      })
-       
+    app.get('/allmedi', async (req, res) => {
+      const result = await MedicineCollection.find().toArray()
+      res.send(result)
+    })
+
+    //// Added selected data in dataBASE
+    app.post('/carts', async (req, res) => {
+      const cartItem = req.body;
+      const result = await SelectedMedicine.insertOne(cartItem);
+      res.send(result);
+    });
+
+    // Get all Selected MEDICINE data from db
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+
+      console.log(email, "vejalllllllllllllllllllllllllllllllllllllllllllllllllllll")
+      const query = { email: email };
+      const result = await SelectedMedicine.find(query).toArray();
+      res.send(result);
+    });
+    // delete SELECTED dataaaaaa
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await SelectedMedicine.deleteOne(query);
+      res.send(result);
+    })
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
   }
@@ -184,8 +191,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Selling is Starting')}  )
-  
-  app.listen(port, () => {
-    console.log(` Selling   is sitting on port ${port}`);
-  })
+  res.send('Selling is Starting')
+})
+
+app.listen(port, () => {
+  console.log(` Selling   is sitting on port ${port}`);
+})
