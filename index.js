4a01891ddcb10e666fc13e45305c1996c7eb0cbd
Asad Atik <asadatik1995@gmail.com>
2024-06-18 12:15:47 +0600

    Advertise Add and Remove Method empliment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const cors = require('cors');
@@ -10,7 +10,6 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -32,6 +31,38 @@ async function run() {
     const MedicineCollection = client.db('FarmaBazar').collection('allMedicine')
     const SelectedMedicine = client.db('FarmaBazar').collection('ordered')
     const userCollection = client.db('FarmaBazar').collection('users')
+    const advertisementCollection = client.db('FarmaBazar').collection('Advertise')
+    const sliderCollection = client.db('FarmaBazar').collection('slider')
+
+ // /////////////////    JWT Api        ///////////////////////////////////
+
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+      console.log(token, user)
+    })
+
+    // middlewares 
+                const verifyToken = (req, res, next) => {
+                  console.log('inside verify token', req.headers.authorization);
+                 if (!req.headers.authorization) {
+                   return res.status(401).send({ message: 'unauthorized access' });
+                     }
+                      const token = req.headers.authorization.split(' ')[1];
+                         jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                     if (err) {
+                       return res.status(401).send({ message: 'unauthorized access' })
+                      }
+                    req.decoded = decoded;
+                            next();
+                    })
+
+               
+                                }
+
+
 
     /////////USER RELETED API
 
@@ -78,7 +109,7 @@ async function run() {
 
 
 
- // post user info
+    // post user info
 
     app.post('/users', async (req, res) => {
       const user = req.body;
@@ -93,14 +124,14 @@ async function run() {
       res.send(result);
     });
     //get 
-    app.get('/users', async (req, res) => {
+    app.get('/users', verifyToken, async (req, res) => {
       const result = await userCollection.find().toArray()
 
       res.send(result)
     })
 
 
- ////CATEGORY ////  
+    ////CATEGORY ////  
 
     // add
     app.post('/category', async (req, res) => {
@@ -181,8 +212,71 @@ async function run() {
       res.send(result);
     })
 
+
+    // ADVIRSMENT 
+        app.post('/advert', verifyToken, async (req, res) => {
+          const info = req.body;
+          const result = await advertisementCollection.insertOne(info);
+          res.send(result);
+      })
+
+      app.get('/advert', verifyToken, async (req, res) => {
+          const result = await advertisementCollection.find().toArray();
+          res.send(result)
+      })
+
+      app.patch('/advert/:id', verifyToken, async (req, res) => {
+          const id = req.params.id;
+          const data = req.body;
+          const query = { _id: new ObjectId(id) };
+          const updateDoc = {
+              $set: {
+                  status: data.status
+              }
+          };
+          const result = await advertisementCollection.updateOne(query, updateDoc);
+          res.send(result);
+      })
+
+      app.get('/advertisement', verifyToken, async (req, res) => {
+          const email = req.query.email;
+          console.log(email);
+          const filter = { sellerEmail: email };
+          const result = await advertisementCollection.find(filter).toArray();
+          res.send(result);
+      })
+
+
+
+   // slider related api
+      
+      
+        app.get('/slider', async (req, res) => {
+          const result = await sliderCollection.find().toArray();
+          res.send(result);
+      })
+
+      app.post('/slider', verifyToken, async (req, res) => {
+          const data = req.body;
+          const result = await sliderCollection.insertOne(data);
+          res.send(result);
+      })
+      app.delete('/sliders/:id', verifyToken, async (req, res) => {
+          const id = req.params.id;
+          const quary = { id: id };
+          const result = await sliderCollection.deleteOne(quary);
+          res.send(result);
+      })
+
+
+
+
+
+
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
+
   }
 }
 run().catch(console.dir);
